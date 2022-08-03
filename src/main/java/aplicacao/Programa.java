e4e9824bcaa50b397eb38ea513ba59f7c46f8ef0
Cristiano Roberto da Silva <javacristiano2021@gmail.com>
2022-08-03 17:47:39 -0300

    RECUPERAR DO BANCO DE DADOS

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -17,22 +17,13 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 
-		em.getTransaction().begin(); // inicia a transação
+		Pessoa p = em.find(Pessoa.class, 2);
 
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-
-		em.getTransaction().commit(); // confirma a transação
-
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
-		em.close();
-		emf.close();
+		System.out.println(p);
 
 		System.out.println("Pronto!!!!!");
+		em.close();
+		emf.close();
 
 	}
 
